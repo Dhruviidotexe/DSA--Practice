1a91f8ee3f6e153a6563a28ddfde6821dec0570b
dhruviverma226-alt <dhruviverma226@gmail.com>
2026-09-18 18:01:25 +0530

    strings count join

---
+++ Strings/strings.py
@@ -0,0 +1,14 @@
+str = "masala chai"
+print(str[0:6])
+print(str[0:10:2])
+# strings are immutable in python
+print(str.replace("masala", "ginger"))
+str = "ginger, ginger ginger tea"
+print (str.count("ginger"))
+
+#join method
+str1 = " ".join(["ginger", "tea"])
+print(len(str))
+
+# /n - newline
+# /t - tab                                                                                                                                                 
